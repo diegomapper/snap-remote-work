3e88620f09f5c5525b947ce5d90a804e8c65e33c
Diego Perales <dperales1313@gmail.com>
2023-05-25 20:57:01 -0400

    Create the Javascript functions for the dropdown menu

---
+++ app.js
@@ -0,0 +1,16 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById('myDropdown');
+    dropdown.classList.toggle('show');
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('#dropbtn')) {
+        const dropdowns = document.getElementsByClassName('dropdown-content');
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
