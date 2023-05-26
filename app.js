6ac4119c94ebc9edd089efa5edf4d5890c0caaf0
Diego Perales <dperales1313@gmail.com>
2023-05-26 11:59:51 -0400

    Create the functionality for the Company dropdown menu

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-function toggleMenuDropdown() {
-    const dropdown = document.getElementById('menuDropdown');
-    const arrowIcon = document.getElementById('arrowIcon')
+// Features Dropdown Menu
+function toggleFeaturesMenuDropdown() {
+    const dropdown = document.getElementById('featuresDropdown');
+    const arrowIcon = document.getElementById('arrowFeatures');
     dropdown.classList.toggle('show');
     arrowIcon.classList.toggle('arrow-up');
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('#dropbtn')) {
-        const dropdowns = document.getElementsByClassName('dropdown-content');
+    if (!event.target.matches('#dropFeatures')) {
+        const dropdowns = document.getElementsByClassName('drop-features');
         const arrowIcons = document.getElementsByClassName('arrow-up');
         for (let i = 0; i < dropdowns.length; i++) {
             const openDropdownMenu = dropdowns[i];
             const arrowIcon = arrowIcons[i];
             if (openDropdownMenu.classList.contains('show')) {
                 openDropdownMenu.classList.remove('show');
+                arrowIcon.classList.remove('arrow-up');
+            }
+        }
+    }
+}
+
+// Company Dropdown Menu
+function toggleCompanyMenuDropdown() {
+    const dropdown = document.getElementById('companyDropdown');
+    const arrowIcon = document.getElementById('arrowCompany');
+    dropdown.classList.toggle('show');
+    arrowIcon.classList.toggle('arrow-up');
+}
+
+window.onclick  = function(event) {
+    if (!event.target.matches('#dropCompany')) {
+        const dropdowns = document.getElementsByClassName('drop-company');
+        const arrowIcons = document.getElementsByClassName('arrow-up');
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openCompanyMenu = dropdowns[i];
+            const arrowIcon = arrowIcons[i];
+            if (openCompanyMenu.classList.contains('show')) {
+                openCompanyMenu.classList.remove('show');
                 arrowIcon.classList.remove('arrow-up');
             }
         }
