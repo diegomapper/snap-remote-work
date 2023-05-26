29c0e88889dadc76053d17c3a7c3dc6c40a218b6
Diego Perales <dperales1313@gmail.com>
2023-05-25 21:41:30 -0400

    Fix the bug in dropdown menu for the Features menu, now the arrow rotates correctly

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 function toggleDropdown() {
     const dropdown = document.getElementById('myDropdown');
+    const arrowIcon = document.getElementById('arrowIcon')
     dropdown.classList.toggle('show');
+    arrowIcon.classList.toggle('arrow-up');
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('#dropbtn')) {
         const dropdowns = document.getElementsByClassName('dropdown-content');
+        const arrowIcons = document.getElementsByClassName('arrow-up');
         for (let i = 0; i < dropdowns.length; i++) {
             const openDropdown = dropdowns[i];
+            const arrowIcon = arrowIcons[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
+                arrowIcon.classList.remove('arrow-up');
             }
         }
     }
